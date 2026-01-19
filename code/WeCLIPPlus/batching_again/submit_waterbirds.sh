54bb53e3118b642231bd04ac6ec4f111b8afa560
ryanlyang <ryan_l_yang@brown.edu>
2026-01-19 01:59:42 -0500

    good news

--- code/WeCLIPPlus/batching_again/submit_waterbirds.sh
+++ code/WeCLIPPlus/batching_again/submit_waterbirds.sh
@@ -2,12 +2,12 @@
 #SBATCH --account=reu-aisocial
 #SBATCH --partition=tier3
 #SBATCH --gres=gpu:1
-#SBATCH --time=4-12:00:00
+#SBATCH --time=3-12:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=64G
-#SBATCH --output=/home/ryreu/guided_cnn/logsWaterbird/waterbirds_%j.out
-#SBATCH --error=/home/ryreu/guided_cnn/logsWaterbird/waterbirds_%j.err
+#SBATCH --output=/home/ryreu/guided_cnn/logsWaterbird/waterbirds_100_seg_%j.out
+#SBATCH --error=/home/ryreu/guided_cnn/logsWaterbird/waterbirds_100_seg_%j.err
 #SBATCH --signal=TERM@120
 
 set -Eeuo pipefail
@@ -23,7 +23,7 @@ export MKL_NUM_THREADS="${SLURM_CPUS_PER_TASK:-1}"
 export NUMEXPR_NUM_THREADS="${SLURM_CPUS_PER_TASK:-1}"
 export PYTHONNOUSERSITE=1
 
-cd /home/ryreu/guided_cnn/waterbirds/LearningToLook/code/WeCLIPPlus
+cd /home/ryreu/guided_cnn/waterbirds/L100/LearningToLook/code/WeCLIPPlus
 export PYTHONPATH="$PWD:${PYTHONPATH:-}"
 
 echo "[$(date)] Host: $(hostname)"
@@ -37,6 +37,6 @@ python -c "import open_clip" 2>/dev/null || {
 
 srun --unbuffered python -u generate_pseudo_masks_waterbirds.py \
   --setup-data \
-  --repo-root "/home/ryreu/guided_cnn/waterbirds/LearningToLook" \
-  --src-img-dir "/home/ryreu/guided_cnn/waterbirds/waterbird_complete95_forest2water2" \
+  --repo-root "/home/ryreu/guided_cnn/waterbirds/L100/LearningToLook" \
+  --src-img-dir "/home/ryreu/guided_cnn/waterbirds/waterbird_1.0_forest2water2" \
   --class-name "bird"
